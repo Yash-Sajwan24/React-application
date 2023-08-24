f82128359652b491f695889c34e484bfc674a5c3
Yash-Sajwan24 <yashsajwan12345@gmail.com>
2023-08-24 08:50:36 +0530

    second page API call

--- react_app/src/pages/SecondPage.tsx
+++ react_app/src/pages/SecondPage.tsx
@@ -1,9 +1,19 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
 import { useNavigate, Outlet } from 'react-router-dom';
+import axios from 'axios';
 // ... other imports
+
+interface Post {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
 
 function SecondPage() {
   const navigate = useNavigate(); // Initialize useNavigate
+  const [data, setData] = useState<Post[]>([]);
 
   useEffect(() => {
     const userDetails = localStorage.getItem('userDetails');
@@ -13,11 +23,39 @@ function SecondPage() {
     }
   }, [navigate]);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+        const jsonData = response.data;
+        setData(jsonData);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const columns: GridColDef[] = [
+    { field: 'id', headerName: 'ID', width: 70 },
+    { field: 'title', headerName: 'Title', width: 300 },
+    { field: 'body', headerName: 'Body', width: 600 },
+  ];
+
+
   return (
     <div>
       <h2>Second Page</h2>
       {/* Render the nested routes */}
       <Outlet />
+      <div style={{ height: 400, width: '100%' }}>
+      <DataGrid rows={data} columns={columns}  initialState={{
+    pagination: {
+      paginationModel: { pageSize: 25, page: 0 },
+    },
+  }} />
+    </div>
     </div>
   );
 }
